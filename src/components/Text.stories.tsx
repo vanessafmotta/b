3af66f06fb0cfc3ccfe60e88abdd536ccfc30f2d
Vanessa Motta <89320692+vanessafmotta@users.noreply.github.com>
2022-10-26 23:23:45 -0300

    .

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -2,46 +2,46 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Text, TextProps } from './Text'
 
 export default {
-    title: 'Components/Text',
-    component: Text,
-    args: {
-        children: 'Lorem ipsum',
-        size: 'md',
-    },
-    argTypes: {
-        size: {
-            option: ['sm', 'md', 'lg'],
-            control: {
-                type: 'inline-radio'
-            }
-        }
-    }
-}as Meta<TextProps>
+	title: 'Components/Text',
+	component: Text,
+	args: {
+		children: 'Lorem ipsum',
+		size: 'md',
+	},
+	argTypes: {
+		size: {
+			option: ['sm', 'md', 'lg'],
+			control: {
+				type: 'inline-radio'
+			}
+		}
+	}
+} as Meta<TextProps>
 
 export const Deafult: StoryObj<TextProps> = {
 
 }
 export const Small: StoryObj<TextProps> = {
-    args: {
-        size: 'sm'
-    }
+	args: {
+		size: 'sm'
+	}
 }
 export const Large: StoryObj<TextProps> = {
-    args: {
-        size: 'lg'
-    }
+	args: {
+		size: 'lg'
+	}
 }
 export const CustomComponent: StoryObj<TextProps> = {
-    args: {
-        asChild: true,
-        children: (
-            <p>Testando</p>
-        )
-    },argTypes: {
-        children: {
-            table: {
-               disable: true, 
-            }
-        }
-    }
+	args: {
+		asChild: true,
+		children: (
+			<p>Text</p>
+		)
+	}, argTypes: {
+		children: {
+			table: {
+				disable: true,
+			}
+		}
+	}
 }
